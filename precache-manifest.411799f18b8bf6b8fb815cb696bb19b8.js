0b96bb56d2221b994d684914207c10d0e5fba243
vampiree007 <vikrantgupta.psit@gmail.com>
2020-08-22 18:03:55 +0530

    Updates

---
+++ precache-manifest.411799f18b8bf6b8fb815cb696bb19b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2a1fcb76b635e9f664c042f98efc72",
+    "url": "/Admin_table/index.html"
+  },
+  {
+    "revision": "cfee587099b1b8d8c894",
+    "url": "/Admin_table/static/css/main.adc6fc80.chunk.css"
+  },
+  {
+    "revision": "3ef5b03279092ccd953c",
+    "url": "/Admin_table/static/js/2.f69288d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Admin_table/static/js/2.f69288d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfee587099b1b8d8c894",
+    "url": "/Admin_table/static/js/main.e2a63419.chunk.js"
+  },
+  {
+    "revision": "6d2909d6af5e17a0e6a3",
+    "url": "/Admin_table/static/js/runtime-main.d9c88dea.js"
+  }
+]);
